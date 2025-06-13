9ce9d907bbbc0dbaedd251540a1bb27423e0e263
Derek (Work) <dbanker@aerospheremonitoring.com>
2025-06-13 13:02:03 -0700

    Fixing Imports

--- src/toml_init/__init__.py
+++ src/toml_init/__init__.py
@@ -13,10 +13,12 @@ from toml_init.exceptions import (
     BlockConflictError,
 )
 from toml_init.validators import register_validator, Validator, CUSTOM_VALIDATORS
+from toml_init.encryption_manager import EncryptionManager
 
 __all__: List[str] = [
     "ConfigManager",
     "main",
+    "EncryptionManager",
     "TomlInitError",
     "MultipleConfigFilesError",
     "InvalidDefaultSchemaError",
